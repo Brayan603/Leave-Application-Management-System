f529456b908f3ac2207b7ff1d6bcfeaf6acd7d9d
Brayan603 <b.malova60@gmail.com>
2026-05-06 13:29:40 +0300

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
-import Leave from "../models/Leave.js";
+import Leave from "../models/LeaveType.js";
 import { protectAdmin, protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
