2d81b273900273c3a72402668cf3ad59436b0c9d
Brayan603 <b.malova60@gmail.com>
2026-05-09 19:54:11 +0300

    Create jobRole.routes.js

---
+++ routes/jobRole.routes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  createJobRole,
+  getJobRoles,
+  getRolesBySubDepartment,
+  updateJobRole,
+  deleteJobRole,
+} = require("../controllers/jobRoleController");
+
+
+// CREATE
+router.post("/", createJobRole);
+
+// GET ALL
+router.get("/", getJobRoles);
+
+// GET BY SUBDEPARTMENT
+router.get(
+  "/subdepartment/:subDepartmentId",
+  getRolesBySubDepartment
+);
+
+// UPDATE
+router.put("/:id", updateJobRole);
+
+// DELETE
+router.delete("/:id", deleteJobRole);
+
+module.exports = router;
